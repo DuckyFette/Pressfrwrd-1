1fdd859f5192ea46b48584472cd173c1dd5c7ee3
Mike <troubledducky@.gmail.com>
2014-01-24 04:58:46 -0700

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,14 +29,16 @@ Pressfrwrd::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: ENV["pressfrwrd.com"],
+    domain: ENV["DOMAIN_NAME"],
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
     }
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.raise_delivery_errors = true
 config.action_mailer.perform_deliveries = true
-
 
 end
 
